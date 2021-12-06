a989efaa713bfc36147cfcbe8447fe1b30f985d4
YoungHyun Cha <gogocha9@naver.com>
2021-12-06 09:51:54 +0900

    백엔드에서 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,4 +1,5 @@
 "use strict";
+
 const output = {
     hello : (req, res) => {
         res.render("home/index");
@@ -8,9 +9,30 @@ const output = {
     },
 };
 
+const users = {
+    id: ["woorimIT", "나개발", "김팀장"],
+    psword: ["1234", "1234", "123456"],
+};
+
 const process = {
     login: (req, res) => {
-        console.log(req.body);
+        const id = req.body.id,
+        psword = req.body.psword;
+
+        if(users.id.includes(id) && users.psword.includes(psword)) {
+            const idx = users.id.indexOf(id);
+            if(users.psword[idx] === psword) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: true,
+            msg: "로그인에 실패하셨습니다."
+        });
+
     },
 };
 
