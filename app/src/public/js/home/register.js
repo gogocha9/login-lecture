5c44b023520d93f4d69ebb6d71e7e5034f39a305
YoungHyun Cha <gogocha9@naver.com>
2021-12-07 14:58:13 +0900

    fs모듈을 통한 로그인 기능 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -24,7 +24,7 @@ function register() {
         method: "POST",
         headers: {
             "Content-Type" : "application/json"
-        },
+        }, 
         body: JSON.stringify(req),
     })
     .then((res) => res.json())
