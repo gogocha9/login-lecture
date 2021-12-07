7cb7a195959fa2f71c411ce5871cc10c1e83e70c
YoungHyun Cha <gogocha9@naver.com>
2021-12-07 11:54:16 +0900

    회원가입 라우팅 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const id = document.querySelector("#id"),
-    name = document.querySelector(".#name");
+    name = document.querySelector("#name"),
     psword = document.querySelector("#psword"),
     confirmPsword = document.querySelector("#confirm-psword"),
     registerBtn = document.querySelector("button");
@@ -9,11 +9,15 @@ const id = document.querySelector("#id"),
 registerBtn.addEventListener("click", register);
 
 function register() {
+
+    // 유효성 검사
+    if(!id.value) return alert("아이디를 입력해주십시오.");
+    if(psword !== confirmPsword) return alert("비밀번호가 일치하지 않습니다.");
+
     const req = {
         id: id.value,
         name: name.value,
         psword: psword.value,
-        confirmPsword: confirmPsword.value,
     };
     
     fetch("/register", {
