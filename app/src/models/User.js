462a02fbba2fcd7e26c9475a208bd49b6d768a16
YoungHyun Cha <gogocha9@naver.com>
2021-12-10 11:27:45 +0900

    AWS RDS와 연동 & dotenv로 환경변수 등록

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -9,23 +9,27 @@ class User {
 
     async login() {
         const client = this.body;
-        const { id, psword } = await UserStorage.getUserInfo(client.id);
+        try {
+            const user = await UserStorage.getUserInfo(client.id);
 
-        if(id) {
-            if(id === client.id && psword === client.psword) {
-                return { success: true };
+            if (user) {
+                if (user.id === client.id && user.psword === client.psword) {
+                    return { success: true };
+                }
+                return { success: false, msg: "비밀번호가 틀렸습니다." };
             }
-            return { success: false, msg: "비밀번호가 틀렸습니다." };
+            return { success: false, msg: "존재하지 않는 아이디입니다." };
+        } catch (err) {
+            return { success: false, msg: err };
         }
-        return { success: false, msg: "존재하지 않는 아이디입니다." };
     }
 
     async register() {
         const client = this.body;
-        try{
-        const response = await UserStorage.save(client);
-        return response;
-        } catch(err) {
+        try {
+            const response = await UserStorage.save(client);
+            return response;
+        } catch (err) {
             return { success: false, msg: err };
         }
     }
