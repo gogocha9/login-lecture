0e6757498fb61cce902455aca0c5f814c70da09a
YoungHyun Cha <gogocha9@naver.com>
2021-12-15 11:44:11 +0900

    디자인 수정

--- app/src/public/js/home/index.js
+++ app/src/public/js/home/index.js
@@ -2,10 +2,29 @@
 
 const menubar = document.querySelector(".menubar");
 const menu = document.querySelector(".menu");
+const close = document.querySelector(".close");
+const wrap = document.querySelector("#wrap");
+const main_menu = document.querySelectorAll(".main_menu");
 
 
 
 menubar.addEventListener("click", () => {
-    menubar.classList.toggle("active");
-    menu.classList.toggle("active");
+    menubar.classList.add("active");
+    menu.classList.add("active");
+    wrap.classList.add("active");
 });
+
+close.addEventListener("click", () => {
+    menubar.classList.remove("active");
+    menu.classList.remove("active");
+    wrap.classList.remove("active");
+});
+
+function activeLink() {
+    main_menu.forEach((item) => 
+        item.classList.remove("active"));
+        this.classList.add("active");
+}
+
+main_menu.forEach((item) => 
+item.addEventListener("click", activeLink));
